1060085f9d965d3efd30000b28bb4ad62bd20efe
oppai <hotsoup.h@gmail.com>
2020-08-26 00:39:35 +0900

    Update interface

--- assets/src/components/Raptime.vue
+++ assets/src/components/Raptime.vue
@@ -3,15 +3,24 @@
     <h1>RaptimeTools</h1>
     <div class="race-list">
       <div v-for="(horse, idx1) in horse_list" :key="idx1">
-        <h2>{{ horse.num || 'x' }}番 {{ horse.name }} / {{ horse.data.info }}</h2>
-        <ul>
-          <h4>データ</h4>
-          <li>平均3ハロン: {{ avgFurlong(horse.data.records) }}sec </li>
-          <h4>過去レース</h4>
-          <li v-for="(record, idx2) in horse.data.records" :key="idx2">
-            {{ record.date }}, {{ record.race_info.type }}, {{ record.race_info.length}}m, {{ record.time }}sec
-          </li>
-        </ul>
+        <h2 class="horse-title">{{ horse.num || 'x' }}番 {{ horse.name }} / {{ horse.data.info }}</h2>
+        <div class="horse-data">
+          <div class="horse-basic-data">
+            <h4>データ</h4>
+            <ul>
+              <li>直近5レースの平均1ハロン: {{ avgFurlong(horse.data.records, 5) }}sec </li>
+              <li>直近10レースの平均1ハロン: {{ avgFurlong(horse.data.records, 10) }}sec </li>
+            </ul>
+          </div>
+          <div class="horse-race-data">
+            <h4>過去レース</h4>
+            <ul>
+              <li v-for="(record, idx2) in horse.data.records" :key="idx2">
+                {{ record.date }}, {{ record.race_info.type }}, {{ record.race_info.length}}m, {{ record.time }}sec
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -34,8 +43,8 @@ export default {
       })
   },
   methods: {
-    avgFurlong(records) {
-      const turfs = records.filter(record => record.race_info.type == "芝").slice(5);
+    avgFurlong(records, num) {
+      const turfs = records.filter(record => record.race_info.type == "芝").slice(num);
       if (turfs.length <= 0) {
         return 0;
       }
@@ -46,7 +55,7 @@ export default {
       }).reduce(
         (sum, current) => sum.plus(current)
       ).dividedBy(turfs.length);
-      return avg.times(3).toFixed(2);
+      return avg.toFixed(2);
     }
   }
 }
@@ -54,6 +63,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.horse-title {
+  margin-top: 50px;
+  margin-bottom: 0px;
+}
+.horse-data {
+  margin-top: 0px;
+  display: inline-flex;
+}
+.horse-basic-data {
+  max-width: 50%;
+}
+.horse-race-data {
+  max-width: 50%;
+}
 h3 {
   margin: 40px 0 0;
 }
