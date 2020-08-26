9bb34e80ade2bae0548c6159b2a44cc834745733
oppai <hotsoup.h@gmail.com>
2020-08-27 01:39:29 +0900

    Update vue component

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
 Vue.use(VueAxios, axios)
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: App },
+  ]
+});
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router
+}).$mount('#app');
